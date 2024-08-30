2bfc59773491e52ec56669c657fefdd0228e2f83
Tieres Bard <tieresbard@hotmail.com>
2024-08-30 11:25:30 -0300

    [FEAT]Adicionando rotas ao router para poder chama-las, testando pelo Postman OK

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,11 +1,26 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
 
-//Estrutura de cada rota da aplicação
+	"github.com/gorilla/mux"
+)
+
+// Estrutura de cada rota da aplicação
 type Route struct {
 	URI          string
 	Method       string
 	Function     func(w http.ResponseWriter, r *http.Request)
 	AuthRequired bool
+}
+
+// Adiciona as rotas ao router
+func ConfigRoutes(m *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		m.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return m
 }
