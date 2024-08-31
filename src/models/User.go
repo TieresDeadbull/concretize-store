4900a738ea7f3934fa0309652f73bcc12f972411
Tieres Bard <tieresbard@hotmail.com>
2024-08-30 23:01:53 -0300

    [FEAT] Preparando para manipulação de banco de dados

---
+++ src/models/User.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+//Estrutura de usuários da aplicação
+type User struct {
+	ID        uint      `json:"id,omitempty`
+	Name      string    `json:"name,omitempty`
+	CPF       string    `json:"cpf,omitempty`
+	Email     string    `json:"email,omitempty`
+	Address   uint      `json:"address,omitempty`
+	Phone     string    `json:"phone,omitempty`
+	CreatedAt time.Time `json:"createdAt,omitempty`
+	EditedAt  time.Time `json:"editedAt,omitempty`
+	DeletedAt time.Time `json:"deletedAt,omitempty`
+}
