4e59f9bf74213a835c98874f22fc75f0f3b83aa1
Tieres Bard <tieresbard@hotmail.com>
2024-09-03 11:56:29 -0300

    [REFACT] Melhorando nomenclatura para posterior compreensao do codigo e adicionando função de criaçao no banco

--- src/models/User.go
+++ src/models/User.go
@@ -8,6 +8,7 @@ type User struct {
 	Name      string    `json:"name,omitempty`
 	CPF       string    `json:"cpf,omitempty`
 	Email     string    `json:"email,omitempty`
+	Password  string    `json: password, omitempty`
 	Address   uint      `json:"address,omitempty`
 	Phone     string    `json:"phone,omitempty`
 	CreatedAt time.Time `json:"createdAt,omitempty`
--- src/repositories/users_repo.go
+++ src/repositories/users_repo.go
@@ -5,16 +5,43 @@ import (
 	"database/sql"
 )
 
-type users struct {
+type UsersRepo struct {
 	db *sql.DB
 }
 
 // Cria novo repositório de usuários
-func NewUsersRepo(db *sql.DB) *users {
-	return &users{db}
+func NewUsersRepo(db *sql.DB) *UsersRepo {
+	return &UsersRepo{db}
 }
 
 // Função que manipula o banco para criação de usuário
-func (u users) CreateUser(user models.User) (uint64, error) {
-	return 0, nil
+func (userRepo UsersRepo) CreateUser(user models.User) (uint64, error) {
+	statement, err := userRepo.db.Prepare(
+		"insert into users(name, cpf, email,password, address, phone) values (?,?,?,?,?,?)",
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec(
+		user.Name,
+		user.CPF,
+		user.Email,
+		user.Password,
+		user.Address,
+		user.Phone,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastInsertID), nil
+
 }
